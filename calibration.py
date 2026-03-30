Fixed orientation and centroid calculation.

calibration.py
@@ -1,6 +1,5 @@
 import math
 import os
-
 import cv2
 import numpy as np
 import json
@@ -12,7 +11,7 @@ print(f"Total pictures found: {len(pictures)}")
 max_images = 5
 board_width = 8
 board_height = 6
-square_length = 36
+square_length = 30
 
 corners = []
 image_points = []
@@ -24,9 +23,6 @@ found = False
 
 for p in pictures:
     if successes >= max_images:
-        pts = image_points[-1].reshape(board_height, board_width, 2)
-        dx = np.diff(pts[:, :, 0], axis=1)
-        avg_square_px = np.mean(np.abs(dx))
         break
 
     img = cv2.imread(p)
@@ -43,7 +39,7 @@ for p in pictures:
     if found:
         print("Chessboard found in:", p)
         corners2 = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-        cv2.drawChessboardCorners(img_clone, board_size, corners2, found)
+        #cv2.drawChessboardCorners(img_clone, board_size, corners2, found)
         image_points.append(corners2.reshape(-1,2))
 
         successes += 1
@@ -51,7 +47,7 @@ for p in pictures:
     else:
         print("Chessboard NOT found in:", p)
 
-    cv2.imshow("Calibration", img_clone)
+    #cv2.imshow("Calibration", img_clone)
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
@@ -106,40 +102,16 @@ undistorted_first = cv2.undistort(img, camera_matrix, dist_coeffs, None, new_mat
 
 # User selects the object of interest in the first undistorted image
 roi = cv2.selectROI("Mark the object", undistorted_first, fromCenter=False) # ENTER or SPACE to confirm, c to cancel
-x, y, w_roi, h_roi = roi
-object_model = undistorted_first[y:y+h_roi, x:x+w_roi]
+x_f, y_f, w_roi, h_roi = roi
+object_model = undistorted_first[y_f:y_f+h_roi, x_f:x_f+w_roi]
 
 # Showing SIFT keypoints on the First marked image
 sift = cv2.SIFT_create()
 kp_first, des_first = sift.detectAndCompute(object_model, None)
 
-cv2.imshow("Object Model", object_model)
+#cv2.imshow("Object Model", object_model)
 cv2.imshow("SIFT Keypoints on First Image", cv2.drawKeypoints(object_model, kp_first, None))
 cv2.waitKey(0)
-
-# Funtion for clicking coordinates on milimeter paper
-points = []
-def click(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        points.append([x,y])
-        print("Clicked:",x,y)
-
-cv2.imshow("image", undistorted_first)
-cv2.setMouseCallback("image", click)
-cv2.waitKey(0)
-
-image_pts = np.array(points, dtype=np.float32)
-
-y = 270
-x = 180
-world_pts = np.array([
-    [0,0],
-    [0,y],
-    [x,y],
-    [x,0]
-], dtype=np.float32)
-
-H, _ = cv2.findHomography(image_pts, world_pts)
 
 for p in pictures_object[1:]:
     print("Processing:", p)
@@ -166,50 +138,71 @@ for p in pictures_object[1:]:
     # Estimating the homography between the first and second image and drawing the matched keypoints and lines between them
     src_pts = np.float32([ kp_first[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
     dst_pts = np.float32([ kp_sec[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
-
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     h_m, w_m = object_model.shape[:2]
     pts = np.float32([[0, 0], [0, h_m - 1], [w_m - 1, h_m - 1], [w_m - 1, 0]]).reshape(-1, 1, 2)
-    dst = cv2.perspectiveTransform(pts,M)
-    dst += (w_m, 0)
-    dst_real = cv2.perspectiveTransform(pts,M)
+    dst_real = cv2.perspectiveTransform(pts, M) # for offset fix
 
+    dst_draw = dst_real + np.array([[[w_m, 0]]])
     img_matches = cv2.drawMatches(object_model, kp_first, undistorted_img, kp_sec, good_matches, None, matchColor=(255,0,0), singlePointColor=(255,0,0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    img_matches = cv2.polylines(img_matches, [np.int32(dst)], True, (255,255,255),1, cv2.LINE_AA)
+    img_matches = cv2.polylines(img_matches, [np.int32(dst_draw)], True, (255,255,255),1, cv2.LINE_AA)
+    p1 = (int(dst_real[0][0][0] + w_m), int(dst_real[0][0][1]))
+    p2 = (int(dst_real[1][0][0] + w_m), int(dst_real[1][0][1]))
+    cv2.arrowedLine(img_matches, p1, p2, (0, 0, 255), 2, tipLength=0.3)
 
     # Centroid
     if len(good_matches) > 0:
-        centroid = np.mean(dst, axis=0)
+        centroid = np.mean(dst_draw, axis=0)
         cv2.circle(img_matches, (int(centroid[0][0]), int(centroid[0][1])), 5, (0,255,0), -1)
     cv2.imshow("Matches", img_matches)
     cv2.waitKey(0)
 
+    # Funtion for clicking coordinates on milimeter paper
+    points = []
+    def click(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            points.append([x,y])
+            print("Clicked:",x,y)
+
+    cv2.imshow("Click corners: 1. upper-left, 2. lower-left, 3. lower-right, 4. upper-right", undistorted_img)
+    cv2.setMouseCallback("Click corners: 1. upper-left, 2. lower-left, 3. lower-right, 4. upper-right", click)
+    cv2.waitKey(0)
+
+    image_pts = np.array(points, dtype=np.float32)
+
+    paper_w = 270
+    paper_h = 180
+    world_pts = np.array([
+        [0,0],
+        [paper_h,0],
+        [paper_h,paper_w],
+        [0,paper_w]
+    ], dtype=np.float32)
+
+    H, _ = cv2.findHomography(image_pts, world_pts)
+
     # Coordinates and orientation
-    pts = dst_real.reshape(4,2)
-    cx = np.mean(pts[:,0])
-    cy = np.mean(pts[:,1])
-    pixel = np.array([[[cx,cy]]], dtype=np.float32)
-    world = cv2.perspectiveTransform(pixel, H)
+    pts_img = dst_real.reshape(4,2)
+    cx = np.mean(pts_img[:,0])
+    cy = np.mean(pts_img[:,1])
+    pixel = np.array([[[cx, cy]]], dtype=np.float32)
+    world = cv2.perspectiveTransform(pixel, H) # centroid in world coordinates
 
     x_mm = world[0][0][0]
     y_mm = world[0][0][1]
 
-    vec = pts[0] - pts[1] # Vector along the X-axis of the object
-    angle_rad = math.atan2(vec[1], vec[0])
-    angle_deg = math.degrees(angle_rad) 
+    pts_real = np.array(pts_img, dtype=np.float32).reshape(-1,1,2)
+    pts_world = cv2.perspectiveTransform(dst_real, H).reshape(4,2) # corners of paper in world coordinates
 
-    # Drawing the orientation arrow
-    start_point = (int(pts[0][0]), int(pts[0][1]))
-    end_point = (int(pts[1][0]), int(pts[1][1]))
-    cv2.line(img_matches, start_point, end_point, (0, 0, 255), 3)
-    cv2.arrowedLine(img_matches, start_point, end_point, (0, 0, 255), 3, tipLength=0.3)
-    cv2.imshow("Orientation", img_matches)
-    cv2.waitKey(0)
+
+    vec = pts_world[1] - pts_world[0]
+    angle_rad = math.atan2(vec[1], vec[0])
+    angle_deg = math.degrees(angle_rad)
+
 
     print("Object position on paper:")
-    print("X =", x_mm, "mm")
-    print("Y =", y_mm, "mm")
+    print("x =", x_mm, "mm")
+    print("y =", y_mm, "mm")
     print("Orientation:", angle_deg, "degrees")
 
 cv2.destroyAllWindows()